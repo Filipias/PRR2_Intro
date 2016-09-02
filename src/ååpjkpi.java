7114346a1f5fe9df003b326d709d177ea1bec9c3
grel14 <te14.elias.grafstrom@umea.nti.se>
2016-09-02 11:19:59 +0200

    awdawdawd

--- src/ååpjkpi.java
+++ src/ååpjkpi.java
@@ -1,7 +1,26 @@
 /**
  * Created by grel14 on 2016-09-02.
  */
-public class ååpjkpi {
+import java.util.Scanner;
 
+public class ååpjkpi {
+    private static Scanner keyboard;
+        public static void main(String[]Args){
+
+            keyboard = new Scanner(System.in);
+            System.out.println("Ange heltal 1 ");
+            String INT1 = keyboard.nextLine();
+
+            System.out.println("Ange heltal 2 ");
+            String INT2 = keyboard.nextLine();
+
+            System.out.println("Ange heltal 3 ");
+            String INT3 = keyboard.nextLine();
+
+            System.out.println("Ange heltal 4 ");
+            String INT4 = keyboard.nextLine();
+
+            System.out.println(INT4 + INT3 + INT2 + INT1);
+        }
     }
 
